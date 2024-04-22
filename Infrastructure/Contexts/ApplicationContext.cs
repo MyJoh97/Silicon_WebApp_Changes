b3e9d84482533afb0d40e8dd2c8c0d91c9bde399
My <my.j97@protonmail.com>
2024-04-22 16:12:29 +0200

    Add project files.

---
+++ Infrastructure/Contexts/ApplicationContext.cs
@@ -0,0 +1,11 @@
+﻿using Infrastructure.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace Infrastructure.Contexts;
+
+public class ApplicationContext(DbContextOptions<ApplicationContext> options) : IdentityDbContext<UserEntity>(options)
+{
+    public DbSet<AddressEntity> Addresses { get; set; }
+}
---
+++ WebApp/Controllers/DefaultController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using WebApp.ViewModels;
+
+namespace WebApp.Controllers;
+
+public class DefaultController(HttpClient httpClient) : Controller
+{
+    private readonly HttpClient _httpClient = httpClient;
+
+    public IActionResult Home()
+    {
+        return View();
+    }
+
+
+    [HttpPost]
+    public async Task<IActionResult> Subscribe(SubscribeViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+            var respons = await _httpClient.PostAsync("https://localhost:7116/api/subscribe", content);
+            if (respons.IsSuccessStatusCode)
+            {
+                TempData["StatusMessage"] = "You are now subscribed";
+            }
+            else if (respons.StatusCode == System.Net.HttpStatusCode.Conflict)
+            {
+                TempData["StatusMessage"] = "You are already subscribed";
+            }
+        }
+        else
+        {
+            TempData["StatusMessage"] = "Invalid email address";
+        }
+
+        return RedirectToAction("Home", "Default", "subscribe");
+    }
+}
